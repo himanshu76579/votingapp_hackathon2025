4d07b52d03654fac478689ed06f1194891853f1f
Amankumar-033 <amanbkp9135@gmail.com>
2025-04-12 16:40:34 +0530

    "Thirrd commit"

--- voting-app/src/App.js
+++ voting-app/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import LoginPage from './components/Loginpage';
 import Home from './components/Home';
 import FingerprintScan from './components/FingerprintScan';
 import States from './components/States';
-
 import CheckElection from './components/CheckElection';
+
 
 function App() {
   return (
     <Router>
-     
       <Navbar />
 
-     
       <Routes>
         <Route path="/" element={<Home />} />
-        //Main hoon baap coder
         <Route path="/login" element={<LoginPage />} />
         <Route path="/fingerprint" element={<FingerprintScan />} />
         <Route path="/vote" element={<States />} />
